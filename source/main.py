4555e44b81586e7d6d91d39267547b0eafffd70c
Szymon Świedrych <szymek_sw@outlook.com>
2022-05-14 11:07:28 +0200

    Reformat code to satisfy PEP requirements

--- source/main.py
+++ source/main.py
@@ -1,10 +1,13 @@
-import source.process_audio as r
-from rich.console import Console
-from rich.prompt import Prompt
 import os
 
+from rich.console import Console
+from rich.prompt import Prompt
+
+import source.process_audio as r
+
+
 def cls():
-    os.system('cls' if os.name=='nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 
 prompt = Prompt()
@@ -15,59 +18,64 @@ audio_setup = r.AudioSetup()
 
 
 def main():
-	output_path = ""
-	user_input = ''
-	debug_enabled = False
-	while user_input != 'q':
-		cls()
-		print()
-		if debug_enabled: console.print("[ Debug enabled ]\n")
-		#todo: poprawić to niżej
-		if output_path != "": console.print(f"Nazwa pliku:\n {output_path} \n")
-		console.print("Podaj, czy chcesz:",
-		"\t(1) Nagrać dźwięk",
-		"\t(2) Odtworzyć dźwięk",
-		"\t[dim]\[d - debug][/]",
-		"\t[dim]\[q - wyjdź][/]", sep="\n")
-		user_input = console.input("> ")
-		print()
-		match user_input:
-			case '1': 
-				output_path = record_audio(output_path)
-			case '2': 
-				if output_path != "": play_audio(output_path)
-				else: console.input(f"Trzeba najpierw nagrać plik audio. {waiter_txt}")
-			case 'l': debug_enabled = not debug_enabled
-			case 'q': break
-			case _: continue
+    output_path = ""
+    user_input = ''
+    debug_enabled = False
+    while user_input != 'q':
+        cls()
+        print()
+        if debug_enabled: console.print("[ Debug enabled ]\n")
+        # todo: poprawić to niżej
+        if output_path != "": console.print(f"Nazwa pliku:\n {output_path} \n")
+        console.print("Podaj, czy chcesz:",
+                      "\t(1) Nagrać dźwięk",
+                      "\t(2) Odtworzyć dźwięk",
+                      "\t[dim]\[d - debug][/]",
+                      "\t[dim]\[q - wyjdź][/]", sep="\n")
+        user_input = console.input("> ")
+        print()
+        match user_input:
+            case '1':
+                output_path = record_audio(output_path)
+            case '2':
+                if output_path != "":
+                    play_audio(output_path)
+                else:
+                    console.input(f"Trzeba najpierw nagrać plik audio. {waiter_txt}")
+            case 'l':
+                debug_enabled = not debug_enabled
+            case 'q':
+                break
+            case _:
+                continue
 
 
 def record_audio(file_name):
-	if file_name == "": file_name = "plik"
-	file_name = prompt.ask("Podaj nazwę pliku: ", default=file_name)
-	audio_setup.chunk = int(prompt.ask("Podaj rozdzielczość: ", default=str(audio_setup.chunk)))
-	console.print(f"Podaj format wejściowy:",
-		"(1) Int8",
-		"(2) Int16",
-		"(3) Int24",
-		"(4) Int32", sep="\n")
-	input_format_chosen = int(prompt.ask("", default=str(audio_setup.input_format)))
-	audio_setup.channel_number = int(prompt.ask(
-		"Wprowadź liczbę kanałów", 
-		choices=["1", "2"], 
-		default="1"))
-	audio_setup.rate = int(prompt.ask("Podaj częstotliwość próbkowania: ", default=str(audio_setup.rate)))
-	console.print("Rozpoczęto nagrywanie")
-	r.record_audio(audio_setup, file_name)
-	return file_name
+    if file_name == "": file_name = "plik"
+    file_name = prompt.ask("Podaj nazwę pliku: ", default=file_name)
+    audio_setup.chunk = int(prompt.ask("Podaj rozdzielczość: ", default=str(audio_setup.chunk)))
+    console.print(f"Podaj format wejściowy:",
+                  "(1) Int8",
+                  "(2) Int16",
+                  "(3) Int24",
+                  "(4) Int32", sep="\n")
+    input_format_chosen = int(prompt.ask("", default=str(audio_setup.input_format)))
+    audio_setup.channel_number = int(prompt.ask(
+        "Wprowadź liczbę kanałów",
+        choices=["1", "2"],
+        default="1"))
+    audio_setup.rate = int(prompt.ask("Podaj częstotliwość próbkowania: ", default=str(audio_setup.rate)))
+    console.print("Rozpoczęto nagrywanie")
+    r.record_audio(audio_setup, file_name)
+    return file_name
 
 
 def play_audio():
-	file_name = prompt.ask("Podaj nazwę pliku: ", default=file_name)
-	audio_setup.chunk = int(console.input(f"Podaj rozdzielczość [{audio_setup.chunk}]: "))
-	console.print("Odtwarzanie...")
-	r.play_audio(audio_setup, file_name)
+    file_name = prompt.ask("Podaj nazwę pliku: ", default=file_name)
+    audio_setup.chunk = int(console.input(f"Podaj rozdzielczość [{audio_setup.chunk}]: "))
+    console.print("Odtwarzanie...")
+    r.play_audio(audio_setup, file_name)
 
 
 if __name__ == '__main__':
-	main()
+    main()
--- source/process_audio.py
+++ source/process_audio.py
@@ -1,71 +1,74 @@
-import pyaudio
 import wave
 
+import pyaudio
+
+
 class AudioSetup:
-	chunk = int()
-	input_format = pyaudio.paInt16
-	channel_number = int()
-	rate = int()
+    chunk = int()
+    input_format = pyaudio.paInt16
+    channel_number = int()
+    rate = int()
 
-	def __init__(self):
-		self.chunk = 1024
-		self.input_format = 2
-		self.channel_number = 1
-		self.rate = 44100
-
-def record_audio(audio_setup : AudioSetup, file_name):
-	chosen_input_format = \
-		[pyaudio.paInt8, pyaudio.paInt16, \
-		pyaudio.paInt24, pyaudio.paInt32][audio_setup.input_format-1]
-	p = pyaudio.PyAudio()
-
-	stream = p.open(
-		format=chosen_input_format,	
-		channels=audio_setup.channel_number,
-		rate=audio_setup.rate,
-		frames_per_buffer=audio_setup.chunk,
-		input=True)
-	
-	audio_input = []
-	try:
-		print("Wciśnij Ctrl+C, aby zakończyć nagrywanie")
-		while True:
-			data = stream.read(audio_setup.chunk)
-			audio_input.append(data)
-	except KeyboardInterrupt:
-		print('Zakończono nagrywanie')
-	stream.stop_stream()
-	stream.close()
-	p.terminate()
-	save_wav_file(audio_input, audio_setup, file_name, p)
+    def __init__(self):
+        self.chunk = 1024
+        self.input_format = 2
+        self.channel_number = 1
+        self.rate = 44100
 
 
-def save_wav_file(audio_input, audio_setup : AudioSetup, file_name : str, p : pyaudio.PyAudio):
-	wf = wave.open(file_name+".wav", 'wb')
-	wf.setnchannels(audio_setup.channel_number)
-	wf.setsampwidth(p.get_sample_size(audio_setup.input_format))
-	wf.setframerate(audio_setup.rate)
-	wf.writeframes(b''.join(audio_input))
-	wf.close()
+def record_audio(audio_setup: AudioSetup, file_name):
+    chosen_input_format = \
+        [pyaudio.paInt8, pyaudio.paInt16, \
+         pyaudio.paInt24, pyaudio.paInt32][audio_setup.input_format - 1]
+    p = pyaudio.PyAudio()
+
+    stream = p.open(
+        format=chosen_input_format,
+        channels=audio_setup.channel_number,
+        rate=audio_setup.rate,
+        frames_per_buffer=audio_setup.chunk,
+        input=True)
+
+    audio_input = []
+    try:
+        print("Wciśnij Ctrl+C, aby zakończyć nagrywanie")
+        while True:
+            data = stream.read(audio_setup.chunk)
+            audio_input.append(data)
+    except KeyboardInterrupt:
+        print('Zakończono nagrywanie')
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    save_wav_file(audio_input, audio_setup, file_name, p)
 
 
-def play_audio(audio_setup : AudioSetup, file_name):
-	audio_file = wave.open(file_name, 'rb')
+def save_wav_file(audio_input, audio_setup: AudioSetup, file_name: str, p: pyaudio.PyAudio):
+    wf = wave.open(file_name + ".wav", 'wb')
+    wf.setnchannels(audio_setup.channel_number)
+    wf.setsampwidth(p.get_sample_size(audio_setup.input_format))
+    wf.setframerate(audio_setup.rate)
+    wf.writeframes(b''.join(audio_input))
+    wf.close()
 
-	p = pyaudio.PyAudio()
 
-	stream = p.open(format=p.get_format_from_width(audio_file.getsampwidth()),
-					channels=audio_file.getnchannels(),
-					rate=audio_file.getframerate(),
-					output=True)
+def play_audio(audio_setup: AudioSetup, file_name):
+    audio_file = wave.open(file_name, 'rb')
 
-	data = audio_file.readframes(audio_setup.chunk)
+    p = pyaudio.PyAudio()
 
-	while data != '':
-		stream.write(data)
-		data = audio_file.readframes(audio_setup.chunk)
+    stream = p.open(format=p.get_format_from_width(audio_file.getsampwidth()),
+                    channels=audio_file.getnchannels(),
+                    rate=audio_file.getframerate(),
+                    output=True)
 
-	stream.stop_stream()
-	stream.close()
+    data = audio_file.readframes(audio_setup.chunk)
 
-	p.terminate()
+    while data != '':
+        stream.write(data)
+        data = audio_file.readframes(audio_setup.chunk)
+
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
