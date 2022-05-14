5111fe3ce3957985a98bf9ad0d52e5b21771a84b
Szymon Świedrych <szymek_sw@outlook.com>
2022-05-14 19:40:30 +0200

    Fix both communication

--- source/test_both.py
+++ source/test_both.py
@@ -5,11 +5,12 @@ import threading
 
 PORT = 2137
 
-audio_setup = (1024, pyaudio.paInt16, 2, 44100)
+audio_setup = (128, pyaudio.paInt16, 2, 44100)
 
-sender_thread = threading.Thread(target=ac.send_audio, args=(audio_setup, ("192.168.0.18", PORT)))
+sender_thread = threading.Thread(target=ac.send_audio, args=(audio_setup, ("192.168.0.19", PORT)))
 receiver_thread = threading.Thread(target=ac.receive_audio, args=(audio_setup, PORT))
 
 receiver_thread.start()
 input("Naciśnij enter aby rozpocząć połączenie")
+print("Rozpoczynam połączenie")
 sender_thread.start()
